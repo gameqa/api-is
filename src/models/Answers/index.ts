0ed58a64b95f7054282c8861d685d1f9c87b406d
njalls <njall16@ru.is>
2021-03-09 14:49:57 +0000

    added todo

--- src/models/Answers/index.ts
+++ src/models/Answers/index.ts
@@ -1,7 +1,14 @@
 import { Schema, model } from "mongoose";
 
 const answerSchema = new Schema(
-	{},
+	{
+		// firstWord
+		// lastWord
+		// submittedBy
+		// questionId
+		// ^ firstWord in range of questions para
+		// ^ lastwordInRange of questions para
+	},
 	{
 		timestamps: true,
 	}

b19fa524e3b8c6b6e351e328b85aa80c3fbc99cf
njalls <njall16@ru.is>
2021-03-16 12:21:15 +0000

    cwt

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import server from "./app";
-import http from "http";
+import cheerio from "cheerio";
+import axios from "axios";
 
 /**
  * Start Express server.
@@ -21,30 +22,89 @@ interface ScrapeData {
 }
 
 interface ArticleScraper {
-	scrapeArticle(): ScrapeData;
+	scrapeArticle(): Promise<ScrapeData>;
 }
 
 class ArticleScraperBase {
 	protected host: string;
 	protected sourceArticleKey: string;
-	protected getUrl() {
-		throw new Error("Not implemented");
-	}
+	protected paragraphs: string[];
+	protected extract: string;
+	protected title: string;
 	public constructor(sourceArticleKey: string) {
 		this.sourceArticleKey = sourceArticleKey;
+		this.paragraphs = [];
 	}
 }
 
 class VisindavefurScraper extends ArticleScraperBase implements ArticleScraper {
-	public scrapeArticle(): ScrapeData {
+	public async scrapeArticle(): Promise<ScrapeData> {
+		const { data } = await axios.get<string>(
+			`https://www.visindavefur.is/svar.php?id=${this.sourceArticleKey}`
+		);
+		const $ = cheerio.load(data);
+		const { children } = $(".article-text").get(0);
+		for (const child of children) {
+			if (child.type !== "text") continue;
+			const text = child.data.replace(/(\n|\t|\r)/g, "");
+			if (!text) continue;
+			this.paragraphs.push(text.trim());
+		}
+
+		this.title = $("h1").get(0).children.pop().data;
 		return {
-			extract: "",
-			title: "",
+			extract: this.paragraphs[0],
+			title: this.title,
 			sourceArticleKey: this.sourceArticleKey,
-			paragraphs: [],
+			paragraphs: this.paragraphs,
 		};
 	}
 }
 
-const data = new VisindavefurScraper("75575").scrapeArticle();
-console.log(data);
+class WikipediaScraper extends ArticleScraperBase implements ArticleScraper {
+	public async scrapeArticle(): Promise<ScrapeData> {
+		const { data } = await axios.get<string>(
+			`https://is.wikipedia.org/wiki/${this.sourceArticleKey}`
+		);
+		const $ = cheerio.load(data);
+		$("p").each((_, element) => {
+			const text = $(element)
+				.text()
+				.replace(/(\n|\t|\r)/g, "");
+			if (!text) return;
+			this.paragraphs.push(text);
+		});
+
+		this.title = $("h1").get(0).children.pop().data;
+		return {
+			extract: this.paragraphs[0],
+			title: this.title,
+			sourceArticleKey: this.sourceArticleKey,
+			paragraphs: this.paragraphs,
+		};
+	}
+}
+class MblScraper extends ArticleScraperBase implements ArticleScraper {
+	public async scrapeArticle(): Promise<ScrapeData> {
+		const { data } = await axios.get<string>(
+			`https://www.mbl.is/${this.sourceArticleKey}`
+		);
+		const $ = cheerio.load(data);
+		const mainLayout = cheerio.load($(".main-layout").get(0));
+		console.log(mainLayout);
+		return {
+			extract: this.paragraphs[0],
+			title: this.title,
+			sourceArticleKey: this.sourceArticleKey,
+			paragraphs: this.paragraphs,
+		};
+	}
+}
+
+new MblScraper(
+	"frettir/innlent/2021/03/16/bandarikjamenn_og_bretar_mjog_spenntir_ad_koma"
+)
+	.scrapeArticle()
+	.then((data) => {
+		console.log(data);
+	});

d04335a295f7203aec4e63e38fb0abf9e707bb88
njalls <njall16@ru.is>
2021-03-16 15:43:12 +0000

    added answers dir, interface for model

---
+++ src/models/Answers/interface.ts
@@ -0,0 +1,16 @@
+import { Document, Model, Types } from "mongoose";
+import { Type } from "typescript";
+
+export interface AnswersInterface extends Document {
+	articleId: Types.ObjectId;
+	paragraphIndex: number;
+	firstWord?: number;
+	lastWord?: number;
+	creationRoundId: Types.ObjectId;
+	answerRoundId?: Types.ObjectId;
+	verificationRoundIds: Types.ObjectId[];
+	verifiedAt?: Date;
+	answeredAt?: Date;
+}
+
+export interface AnswersCollectionInterface extends Model<AnswersInterface> {}

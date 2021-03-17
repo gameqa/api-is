104759032a0ccbf3228d11d74dc653783b0d5708
njalls <njall16@ru.is>
2021-03-17 16:33:13 +0000

    added empty test suite for game rounds

---
+++ src/models/GameRounds/__test__/GameRounds.unit-test.ts
@@ -0,0 +1,8 @@
+describe("Creating gamerounds", () => {
+	describe("the initial value of currentRound", () => {
+		it("should have a default value of 0 when no val passed in", async (done) => {
+			expect(3).toBe(3);
+			done();
+		});
+	});
+});

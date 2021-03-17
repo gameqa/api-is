71799313eae1167d88203b262e7f9f12554110d6
njalls <njall16@ru.is>
2021-03-17 08:41:47 +0000

    article source identifier unique

--- src/models/ArticleSources/__tests__/ArticleSources.unit-test.ts
+++ src/models/ArticleSources/__tests__/ArticleSources.unit-test.ts
@@ -1,4 +1,4 @@
-import { ArticleSources } from "../../";
+import { ArticleSources, ArticleSourcesInterface } from "../../";
 
 const validArticle1 = {
 	identifier: "__visir__",
@@ -8,6 +8,19 @@ const validArticle2 = {
 	identifier: "__mbl__",
 };
 
+let source: ArticleSourcesInterface;
+
+beforeEach(async (done) => {
+	try {
+		await ArticleSources.findByIdAndDelete(source._id);
+	} catch (e) {
+		//
+	} finally {
+		//
+	}
+	return done();
+});
+
 describe("Creating an ArticleSource", () => {
 	describe("Selecting an identifier", () => {
 		it("It should fail without identifier", async (done) => {
@@ -16,6 +29,23 @@ describe("Creating an ArticleSource", () => {
 					await ArticleSources.create({
 						...validArticle1,
 						identifier: undefined,
+					});
+				} catch (error) {
+					throw new Error("Failed test");
+				}
+			};
+			await expect(shouldFail()).rejects.toEqual(
+				new Error("Failed test")
+			);
+			done();
+		});
+		it("should fail if not unique", async (done) => {
+			source = await ArticleSources.create(validArticle1);
+			const shouldFail = async () => {
+				try {
+					await ArticleSources.create({
+						...validArticle2,
+						identifier: validArticle1.identifier,
 					});
 				} catch (error) {
 					throw new Error("Failed test");
--- src/models/ArticleSources/index.ts
+++ src/models/ArticleSources/index.ts
@@ -1,14 +1,20 @@
 import { Document, model, Model, Schema } from "mongoose";
+import {
+	ArticleSourcesCollectionInterface,
+	ArticleSourcesInterface,
+} from "./interface";
 
 const articleSourcesSchema = new Schema({
 	identifier: {
 		type: String,
 		required: true,
+		unique: true,
 	},
 });
 
-export const ArticleSources = model(
-	"articlesources",
-	articleSourcesSchema,
-	"articlesources"
-);
+export const ArticleSources = model<
+	ArticleSourcesInterface,
+	ArticleSourcesCollectionInterface
+>("articlesources", articleSourcesSchema, "articlesources");
+
+export * from "./interface";

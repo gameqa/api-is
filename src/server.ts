d4276d297cc3403be208ef151a6b897582199857
njalls <njall16@ru.is>
2021-03-17 16:29:06 +0000

    cwt

--- src/server.ts
+++ src/server.ts
@@ -16,3 +16,10 @@ server.listen(server.get("port"), () => {
 Articles.findArticleByUrl("https://www.visindavefur.is/svar.php?id=106", true)
 	.then((res) => console.log(res))
 	.catch((e) => console.log(e));
+
+/**
+ * TODO: make the functionality be like below, not findArticleByUrl
+ */
+
+// http://api.spurningar.is/api/v1/article_sources/__visir__/106
+// http://api.spurningar.is/api/v1/articles/web_search?query_string="asdf"

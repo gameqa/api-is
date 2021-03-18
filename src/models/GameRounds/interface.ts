f37153c2215fb2f8014c58fd38117b410585309e
njalls <njall16@ru.is>
2021-03-18 09:15:01 +0000

    added type declerations for game round tasks

--- src/models/GameRounds/interface.ts
+++ src/models/GameRounds/interface.ts
@@ -1,11 +1,27 @@
 import { Document, Model, Types } from "mongoose";
 
+type TaskTypes =
+	| "ask-question"
+	| "qa-question"
+	| "submit-article"
+	| "select-span"
+	| "qa-answer-span"
+	| "done";
+
+interface TaskInfo {}
+
 export interface GameRoundsInterface extends Document {
 	userId: Types.ObjectId;
 	currentRound: number;
 	totalRounds: number;
 	completedAt?: Date;
+	getTask: () => Promise<TaskInfo>;
+	completeRound: () => Promise<TaskInfo>;
 }
 
 export interface GameRoundsCollectionInterface
-	extends Model<GameRoundsInterface> {}
+	extends Model<GameRoundsInterface> {
+	getByUserId: (
+		userId: Types.ObjectId | string
+	) => Promise<GameRoundsInterface>;
+}

e360b28f7f499e6c962837bcfdcce86b846b8d41
njall skarphedinsson <njallis@gmail.com>
2021-03-29 13:21:48 +0000

    fixed getPublic

--- src/routes/api/v1/users/current/index.ts
+++ src/routes/api/v1/users/current/index.ts
@@ -5,7 +5,7 @@ import { CurrentUserRequest } from "./interface";
  */
 export default async (req: CurrentUserRequest, res: Response) => {
 	try {
-		res.send(req.body.user.getCurrent());
+		res.send(req.body.user.getPublic());
 	} catch (error) {
 		res.status(400).send({ message: error.message });
 	}
--- src/routes/api/v1/users/current/interface.ts
+++ src/routes/api/v1/users/current/interface.ts
@@ -2,5 +2,7 @@ import { Request } from "express";
 import { UserInterface } from "../../../../../models";
 
 export interface CurrentUserRequest extends Request {
-	user: UserInterface;
+	body: {
+		user: UserInterface;
+	};
 }

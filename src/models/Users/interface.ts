c9b33cadc7e94edd35d898a43e4a8bdcd4144ffc
njall skarphedinsson <njallis@gmail.com>
2021-04-27 11:15:24 +0000

    fixed interface bug

--- src/models/Users/interface.ts
+++ src/models/Users/interface.ts
@@ -40,7 +40,7 @@ export interface UserRegisterInfo {
 	email: string;
 	username: string;
 	password: string;
-	password2: string;
+	password2?: string;
 }
 
 export interface UserCollectionInterface extends Model<UserInterface> {

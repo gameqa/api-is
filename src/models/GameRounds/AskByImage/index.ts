bea7c973a2c1685a7bbfe8c899c9c38627cd05e6
njall skarphedinsson <njallis@gmail.com>
2021-06-22 13:58:28 +0000

    cwt

--- src/models/GameRounds/AskByImage/index.ts
+++ src/models/GameRounds/AskByImage/index.ts
@@ -1,73 +1,154 @@
 import { ImageInfo } from "../interface";
 
-
 const images: ImageInfo[] = [
 	{
 		url: "https://m.psecn.photoshelter.com/img-get2/I0000oKx7irZcGXE/sec=/fit=1200x1200/I0000oKx7irZcGXE.jpg",
 		subject_tf: "Búsáhaldabyltinguna",
-    },
-    {
+	},
+	{
 		url: "https://www.thesun.co.uk/wp-content/uploads/2019/11/NINTCHDBPICT000537721299-1.jpg",
 		subject_tf: "Berlínarmúrinn",
-    },
-    {
+	},
+	{
 		url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Jacques-Louis_David_017.jpg/1200px-Jacques-Louis_David_017.jpg",
 		subject_tf: "Napóleon",
-    },
-    {
+	},
+	{
 		url: "https://i0.wp.com/grapevine.is/wp-content/uploads/Owning-It-Emmsje-Gauti-by-Axel-Sigurearson.jpg?quality=99",
 		subject_tf: "Emmsjé Gauta",
-    },
-    {
+	},
+	{
 		url: "https://cdn.mbl.is/frimg/1/13/80/1138015.jpg",
 		subject_tf: "Apann í Eden",
-    },
-    {
+	},
+	{
 		url: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Eyjafjallaj%C3%B6kull_by_Terje_S%C3%B8rgjerd.jpg",
 		subject_tf: "Eldgos",
-    },
-    {
+	},
+	{
 		url: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Eyjafjallaj%C3%B6kull_by_Terje_S%C3%B8rgjerd.jpg",
 		subject_tf: "Eyjafjallajökul",
 	},
-    {
+	{
 		url: "https://m.media-amazon.com/images/M/MV5BOWIzMWYwODItNjRjNC00N2VhLWI2NGItMzM0YTU1ZTY3NTBlXkEyXkFqcGdeQXVyNDE1MjQ4MTk@._V1_.jpg",
 		subject_tf: "Michael Jackson",
 	},
-    {
+	{
 		url: "https://live.staticflickr.com/65535/49937327958_aa0b3c0a44_b.jpg",
 		subject_tf: "Gísla Martein",
 	},
-    {
+	{
 		url: "https://upload.wikimedia.org/wikipedia/is/5/59/Sodoma_reykjavik_vhs.jpg",
 		subject_tf: "Sódóma Reykjavík",
 	},
-    {
+	{
 		url: "https://www.ml.is/wp-content/uploads/2020/12/Modruvallabok-Medium.jpg",
 		subject_tf: "Njálssögu",
 	},
-    {
+	{
 		url: "https://www.forseti.is/media/6349/04-forseti-%C3%ADslands.jpg",
 		subject_tf: "Vigísi Finnbogadóttur",
 	},
-    {
+	{
 		url: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Katr%C3%ADn_Jakobsd%C3%B3ttir_%28cropped%29.jpg",
 		subject_tf: "Katrín Jakobsdóttur",
 	},
-    {
+	{
 		url: "https://cdn.britannica.com/18/193318-050-BA5E2B8A/Melania-Trump-2016.jpg",
 		subject_tf: "Melaniu Trump",
 	},
-    {
+	{
 		url: "https://i2.wp.com/mishmoments.com/wp-content/uploads/2016/12/Seljalandsfoss-Waterfall-South-Iceland-94A2.jpg?fit=533%2C800&ssl=1",
 		subject_tf: "Seljalandsfoss",
 	},
-    {
+	{
 		url: "https://experiencenature.com.au/wp-content/uploads/2021/01/AJM3098.jpg",
 		subject_tf: "Trufflur",
 	},
+	{
+		url: "https://cdn.pixabay.com/photo/2015/04/23/16/40/tower-736316_960_720.jpg",
+		subject_tf: "Tíma",
+	},
+	{
+		url: "https://cdn.pixabay.com/photo/2015/04/23/16/40/tower-736316_960_720.jpg",
+		subject_tf: "London",
+	},
+	{
+		url: "https://cdn.mbl.is/frimg/1/13/43/1134361.jpg",
+		subject_tf: "Herra Hnetusmjör",
+	},
+	{
+		url: "https://cdn.mbl.is/frimg/1/13/43/1134361.jpg",
+		subject_tf: "íslenskt rapp",
+	},
+	{
+		url: "https://cdn.mbl.is/frimg/1/13/43/1134361.jpg",
+		subject_tf: "íslenskan rappara",
+	},
+	{
+		url: "https://cdn.the-scientist.com/assets/articleNo/65517/iImg/30879/kari-stefansson.png",
+		subject_tf: "Kára Stefánsson",
+	},
+	{
+		url: "https://cdn.the-scientist.com/assets/articleNo/65517/iImg/30879/kari-stefansson.png",
+		subject_tf: "Íslenska erfðagreiningu",
+	},
+	{
+		url: "https://www.vb.is/media/cache/2b/8a/2b8a50e6338d1143da785f17aebfed0d.jpg",
+		subject_tf: "Borgartún",
+	},
+	{
+		url: "https://www.vb.is/media/cache/2b/8a/2b8a50e6338d1143da785f17aebfed0d.jpg",
+		subject_tf: "einhverja íslenska byggingu",
+	},
+	{
+		url: "https://www.visitmyvatn.is/static/files/htstor-0131.jpg",
+		subject_tf: "Mývatn",
+	},
+	{
+		url: "https://www.visitmyvatn.is/static/files/htstor-0131.jpg",
+		subject_tf: "norðurland",
+	},
+	{
+		url: "https://www.stjornarradid.is/media/velferdarraduneyti-media/media/myndabanki_VEL/Althingishusid_e.jpg",
+		subject_tf: "Alþingi",
+	},
+	{
+		url: "https://live.staticflickr.com/938/42941892594_a308da17a9_b.jpg",
+		subject_tf: "Menntaskólann í Reykjavík",
+	},
+	{
+		url: "https://www.popsci.com/app/uploads/2019/03/18/7WNRRCTHIOTCHSLWMSROL4O2WM.jpg",
+		subject_tf: "gæsalappir",
+	},
+	{
+		url: "https://www.popsci.com/app/uploads/2019/03/18/7WNRRCTHIOTCHSLWMSROL4O2WM.jpg",
+		subject_tf: "endur",
+	},
+	{
+		url: "https://www.popsci.com/app/uploads/2019/03/18/7WNRRCTHIOTCHSLWMSROL4O2WM.jpg",
+		subject_tf: "fugla",
+	},
+	{
+		url: "https://upload.wikimedia.org/wikipedia/commons/a/ac/Hands-Fingers-Crossed.jpg",
+		subject_tf: "fingur",
+	},
+	{
+		url: "https://upload.wikimedia.org/wikipedia/commons/a/ac/Hands-Fingers-Crossed.jpg",
+		subject_tf: "hendur",
+	},
+	{
+		url: "https://upload.wikimedia.org/wikipedia/commons/a/ac/Hands-Fingers-Crossed.jpg",
+		subject_tf: "lygar",
+	},
+	{
+		url: "https://cdn.britannica.com/94/133594-050-8F09C2FF/Jon-Sigurdsson-statue-Reykjavik-Ice.jpg",
+		subject_tf: "Jónas Sigurðsson",
+	},
+	{
+		url: "https://cdn.britannica.com/94/133594-050-8F09C2FF/Jon-Sigurdsson-statue-Reykjavik-Ice.jpg",
+		subject_tf: "íslensk skáld",
+	},
 ];
 
-export const getImage = () => 
-    images[Math.floor(Math.random() * images.length)];
-
+export const getImage = () => images[Math.floor(Math.random() * images.length)];

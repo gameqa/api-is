13ddce9580b6d1ddbf97d4efcc82936de551d5e1
Breki Gudmundsson <brekigudmundsson@Brekis-MacBook-Air.local>
2021-06-28 10:09:21 +0000

    usersPerday

--- src/routes/api/charts/index.ts
+++ src/routes/api/charts/index.ts
@@ -1,10 +1,16 @@
 import answersPerDay from "./answersPerDay";
 import { RouteBuilder } from "../../utils";
+import usersPerDay from "./usersPerDay";
 
 export default RouteBuilder.routerForEndpoints([
 	{
 		route: "/answers_per_day",
 		controller: answersPerDay,
+		method: "get",
+	},
+	{
+		route: "/users_per_day",
+		controller: usersPerDay,
 		method: "get",
 	},
 ]);

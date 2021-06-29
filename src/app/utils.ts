388ba14c1138ea1728478ad97a0bfec2e169f6a3
njall skarphedinsson <njallis@gmail.com>
2021-06-29 16:41:09 +0000

    cwt

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -83,35 +83,7 @@ export default class AppUtils {
 				useCreateIndex: true,
 				useUnifiedTopology: true,
 			});
-			// console.log("info", "Connected to MongoDB");
-
-			// const PAGE_SIZE = 1;
-			// // query users
-			// const users = await Users.find({
-			// 	"pushNotificationTokens.0": { $exists: true },
-			// });
-			// // store as variable
-			// const userCount = users.length;
-			// // send floor(userCount / PAGE_SIZE) requests with foor loop
-			// let count = 0;
-			// for (let i = 125; i * PAGE_SIZE < userCount; i++) {
-			// 	// get items in page
-			// 	const sublist = users.slice(i * PAGE_SIZE, (i + 1) * PAGE_SIZE);
-
-			// 	// map to interface expo understands
-			// 	const payloadObjects = sublist.map((user) => ({
-			// 		to: user.pushNotificationTokens,
-			// 		title: "Veistu svarið?",
-			// 		body: `Hvað eru margir lundar á Íslandi? Þetta er ein af mörgum spurningum í appinu sem þarfnast svars. Getur þú hjálpað okkur?`,
-			// 	}));
-			// 	// console.log(payloadObjects);
-			// 	// send to expo
-			// 	await axios.post(
-			// 		"https://exp.host/--/api/v2/push/send",
-			// 		payloadObjects
-			// 	);
-			// 	console.log("Sent batch", i);
-			// }
+		
 		} catch (error) {
 			console.log(
 				"error",

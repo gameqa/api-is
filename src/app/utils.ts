55dc4ab0d93aa90b0e4a273a7fc2df4017073a25
Breki Gudmundsson <brekigudmundsson@Brekis-MacBook-Air.local>
2021-06-29 16:01:36 +0000

    push notification

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -84,12 +84,39 @@ export default class AppUtils {
 				useUnifiedTopology: true,
 			});
 			console.log("info", "Connected to MongoDB");
-		
+
+			// const PAGE_SIZE = 35;
+			// // query users
+			// const users = await Users.find({
+			// 	"pushNotificationTokens.0": { $exists: true },
+			// });
+			// // store as variable
+			// const userCount = users.length;
+			// // send floor(userCount / PAGE_SIZE) requests with foor loop
+			// let count = 0;
+			// for (let i = 0; i * PAGE_SIZE < userCount; i++) {
+			// 	// get items in page
+			// 	const sublist = users.slice(i * PAGE_SIZE, (i + 1) * PAGE_SIZE);
+
+			// 	// map to interface expo understands
+			// 	const payloadObjects = sublist.map((user) => ({
+			// 		to: user.pushNotificationTokens,
+			// 		title: "Veistu svarið?",
+			// 		body: `Hvað eru margir lundar á Íslandi? Þetta er ein af mörgum spurningum í appinu sem þarfnast svars. Getur þú hjálpað okkur?`,
+			// 	}));
+			// 	// console.log(payloadObjects);
+			// 	// send to expo
+			// 	await axios.post(
+			// 		"https://exp.host/--/api/v2/push/send",
+			// 		payloadObjects
+			// 	);
+			// 	console.log("Sent batch", i);
+			// }
 		} catch (error) {
 			console.log(
 				"error",
 				"MongoDB connection error. Please make sure MongoDB is running. " +
-					error
+					JSON.stringify(error?.response?.data)
 			);
 		}
 	};
--- src/server.ts
+++ src/server.ts
@@ -91,7 +91,7 @@ schedule.scheduleJob("00 20 * * *", async function () {
 		// send floor(userCount / PAGE_SIZE) requests with foor loop
 		for (let i = 0; i * PAGE_SIZE < userCount; i++) {
 			// get items in page
-			const sublist = users.splice(i * PAGE_SIZE, (i + 1) * PAGE_SIZE);
+			const sublist = users.slice(i * PAGE_SIZE, (i + 1) * PAGE_SIZE);
 			// map to interface expo understands
 			const payloadObjects = sublist.map((user) => ({
 				to: user.pushNotificationTokens,

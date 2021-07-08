04d27d2e6965099787f0bfd92cf83b15f4d82684
Breki Gudmundsson <brekigudmundsson@Brekis-MacBook-Air.local>
2021-07-08 12:56:09 +0000

    fixed async bug in route

--- src/routes/api/v1/answers/patch/index.ts
+++ src/routes/api/v1/answers/patch/index.ts
@@ -8,13 +8,14 @@ import { PatchByIdRequest } from "./interface";
  */
 export default async (req: PatchByIdRequest, res: Response) => {
 	try {
-		const body = _.pick(req.body, [
-			"seenByQuestionerAt",
-		]);
-		const doc = await Answers.findByIdAndUpdate(req.body.answer._id, { $set: body }, { new: true });
-		res.send(doc.toPublic());
+		const body = _.pick(req.body, ["seenByQuestionerAt"]);
+		const doc = await Answers.findByIdAndUpdate(
+			req.body.answer._id,
+			{ $set: body },
+			{ new: true }
+		);
+		res.send(await doc.toPublic());
 	} catch (error) {
-		console.log(error.message)
 		res.status(400).send({ message: error.message });
 	}
 };

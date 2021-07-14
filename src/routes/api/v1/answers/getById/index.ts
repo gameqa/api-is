cf0d5615b02058b8530452778c698dc41d24af78
njall skarphedinsson <njallis@gmail.com>
2021-07-14 13:08:32 +0000

    Added cache controol

--- src/routes/api/v1/answers/getById/index.ts
+++ src/routes/api/v1/answers/getById/index.ts
@@ -6,11 +6,14 @@ import { GetByIdRequest } from "./interface";
  */
 export default async (req: GetByIdRequest, res: Response) => {
 	try {
-		const answer = await req.body.answer.toPublic();
-		res.send(answer);
+		const answer = await req.body.answer;
+		const publicViewOfAnswer = answer.toPublic();
+		const cacheAge = !!answer.answeredAt ? "86400" : "300";
+		res
+			.set("Cache-Control", `public, max-age=${cacheAge}`)
+			.send(publicViewOfAnswer);
 	} catch (error) {
-
-		console.log(error.message)
+		console.log(error.message);
 		res.status(400).send({ message: error.message });
 	}
 };

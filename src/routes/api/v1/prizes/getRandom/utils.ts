808a1583a248ef0090e81e902e0b5a0e5510b6dc
njall skarphedinsson <njallis@gmail.com>
2021-07-16 09:58:47 +0000

    cwt

--- src/routes/api/v1/prizes/getRandom/utils.ts
+++ src/routes/api/v1/prizes/getRandom/utils.ts
@@ -1,8 +1,8 @@
 import prizes from "../prizes.json";
 
 export const getRandomPrize = () => {
-    const prizeArray = prizes.reduce((pv, cv) => 
-        [...pv, ...cv.prizes]
-    , [])
-    return prizeArray[Math.floor(Math.random() * prizeArray.length)];
-}
+	const prizeArray = prizes
+		.reduce((pv, cv) => [...pv, ...cv.prizes], [])
+		.filter((prize) => prize.available);
+	return prizeArray[Math.floor(Math.random() * prizeArray.length)];
+};

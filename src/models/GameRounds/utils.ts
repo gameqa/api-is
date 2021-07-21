215ecffa21a468c080b8eb66f1b45e6d3c001396
njall skarphedinsson <njallis@gmail.com>
2021-07-21 13:10:08 +0000

    commented getRoundsForUserLevel

--- src/models/GameRounds/utils.ts
+++ src/models/GameRounds/utils.ts
@@ -1,8 +1,24 @@
 export const DEFAULT_GAME_ROUNDS = 10;
 
-export const getRoundsForUserLevel = (userLevel: number) => {
+/**
+ * Maps a natural number representing user level
+ * to another natural number representing the total rounds
+ * needed to complete in order to finish a game round at
+ * that level
+ *
+ * Function is 2.5 * x^1.1 where x is the user level
+ *
+ * @param {number} userLevel the users current level (works for 1, 2, 3, ...)
+ * @returns {number} total rounds for a game round at this level
+ */
+export const getRoundsForUserLevel = (userLevel: number): number => {
+	// declare consts
 	const EXP = 1.1;
-	if (userLevel < 1) throw new Error("User level can not be negative");
 	const SCALE = 2.5;
+
+	// validate input
+	if (userLevel < 1) throw new Error("User level must be positive");
+
+	// calculate output
 	return Math.floor(SCALE * Math.pow(userLevel, EXP));
 };

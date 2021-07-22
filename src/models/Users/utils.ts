505efd876bda2eb405061bee97ca9fe5f1930d2b
njall skarphedinsson <njallis@gmail.com>
2021-07-22 14:05:24 +0000

    added documentation

--- src/models/Users/utils.ts
+++ src/models/Users/utils.ts
@@ -8,7 +8,7 @@ export const MIN_PW_LENGTH = 5;
 export const VERIFICATION_CODE_LENGTH = 6;
 export const RESET_PASSWORD_CODE_LENGTH = 8;
 export const RESET_PASSWORD_MAX_GUESS_COUNT = 3;
-export const RESET_PASSWORD_TOKEN_RANDOM_NUM_LENGTH = 20;
+export const RESET_PASSWORD_TOKEN_RANDOM_NUM_LENGTH = 30;
 //                                                ms  * s  * m  * h
 export const RESET_PASSWORD_CODE_TIME_PERIOD_LENGTH = 1000 * 60 * 60 * 2;
 export const RESET_PASSWORD_TOKEN_TIME_PERIOD_LENGTH = 1000 * 60 * 5;

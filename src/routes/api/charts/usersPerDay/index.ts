f76abceb212a57f94d983e8cd08f35de907640de
Breki Gudmundsson <brekigudmundsson@Brekis-MacBook-Air.local>
2021-07-28 14:37:49 +0000

    refactored

--- src/routes/api/charts/usersPerDay/index.ts
+++ src/routes/api/charts/usersPerDay/index.ts
@@ -1,49 +1,28 @@
 import { Users } from "../../../../models";
 import { Request, Response } from "express";
+import * as Utils from "./utils";
+import * as Declerations from "./declerations";
 
+/**
+ * @verb GET
+ * @endpoint /api/charts/users_per_day
+ * @description returns an array of objects that represent
+ *     users count per day. Can accept a list of boolean
+ *     query arguments. If invalid arguments are presented,
+ *     then it will respond with error.
+ *
+ * @auth none
+ * @example
+ *     GET /api/charts/users_per_day \
+ *     --data { }
+ */
 export default async (req: Request, res: Response) => {
 	try {
-		const allowedQueryKeys = ["hiscoreRank", "level", "shadowBanned"];
-
-		const queryObject: { [key: string]: any } = { ...req.query };
-
-		for (const key in queryObject)
-			if (!allowedQueryKeys.includes(key))
-				throw new Error(`${key} is not a valid query key`);
-
-		if (queryObject.shadowBanned) {
-			queryObject.shadowBanned =
-				queryObject.shadowBanned === "false" ? false : true;
-		}
-
-		console.log("GETTING USERS INFO: " + new Date().toISOString());
-		const results = await Users.aggregate([
-			{ $match: queryObject },
-			{
-				$project: {
-					date: {
-						$dateToString: {
-							format: "%Y-%m-%d",
-							date: "$createdAt",
-						},
-					},
-				},
-			},
-			{
-				$group: {
-					_id: { date: "$date" },
-					count: { $sum: 1 },
-				},
-			},
-		]);
-		res.send(
-			results
-				.map((item) => ({
-					count: item.count,
-					date: new Date(Date.parse(item._id.date)),
-				}))
-				.sort((a, b) => a.date.getTime() - b.date.getTime())
+		const queryObject = Utils.getQueryObject(
+			req.query as Declerations.StringToString
 		);
+
+		res.send(await Utils.getUsersPerDay(queryObject));
 	} catch (error) {
 		res.status(500).send({
 			message: error.message,

83e93e8305e4c5fc396aab4584dabfccc9b6caba
njall skarphedinsson <njallis@gmail.com>
2021-07-28 13:00:38 +0000

    added documentation for get questions  by day

--- src/routes/api/charts/questionsPerDay/utils.ts
+++ src/routes/api/charts/questionsPerDay/utils.ts
@@ -1,11 +1,19 @@
 import { Questions } from "../../../../models";
 import * as Decleration from "./declerations";
 
+/**
+ * Queries questions in MongoDB according to a given
+ * query object. Then it aggregates them by
+ * day
+ *
+ * @param queryObject query the questions before aggregation by
+ *    info in this object
+ */
 export const getQuestionsPerDay = async (
 	queryObject: Decleration.QueryObject
 ) => {
 	const results = await Questions.aggregate([
-		{ $match: {} },
+		{ $match: queryObject },
 		{
 			$addFields: {
 				createdAt: { $toDate: "$_id" },
@@ -36,14 +44,36 @@ export const getQuestionsPerDay = async (
 		.sort((a, b) => a.date.getTime() - b.date.getTime());
 };
 
+// allowed keys in query
 export const ALLOWED_QUERY_KEYS = [
 	"archived",
 	"isImpossible",
 	"isDisqualified",
 ];
+
+// memory cache key
 export const CHACHE_KEY = "questions:per:day";
+
+// store the results in cache for this many secnods
 export const CACHE_DURATION_SECONDS = 240;
 
+/**
+ * Maps an object (from express request query)
+ * which has boolean values as strings to objects
+ * with actual boolean values
+ *
+ * For exmaple {archived: 'true'}
+ *
+ * This function does two things
+ * i) validates that the query parameters
+ *    are allowed
+ * i) changes values from string to boolean
+ *    that is 'true' to true and 'false' to false
+ *
+ * @param requestQuery the .query object on Express.Request
+ * @returns {Decleration.QueryObject} an object which maps allowed
+ *    attributes to primitive boolean values
+ */
 export const getQueryObject = (
 	requestQuery: Decleration.StringToString
 ): Decleration.QueryObject => {

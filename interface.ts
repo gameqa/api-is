4dffd8edcf7f23b4de258ba7a1418a2177354310
njall skarphedinsson <njallis@gmail.com>
2021-07-29 12:41:29 +0000

    removed files

--- interface.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-import { AnswersInterface, UserInterface } from "../../../../../models";
-
-export interface GetByIdRequest extends Request {
-	body: {
-		user: UserInterface;
-		answer: AnswersInterface;
-	};
-}

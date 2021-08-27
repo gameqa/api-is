82c8e78c74b1b846c0525e91b3af45d91afc965e
Brekig <42963322+Brekig@users.noreply.github.com>
2021-08-27 20:55:40 +0000

    get random prize advertisement adjusted to prizes stored in database

--- src/routes/api/v1/prizes/getRandom/index.ts
+++ src/routes/api/v1/prizes/getRandom/index.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 import { getRandomPrize } from "./utils";
 
 export default async (req: Request, res: Response) => {
-	const prize = getRandomPrize();
+	const prize = await getRandomPrize();
 	res.send(prize);
 };
--- src/routes/api/v1/prizes/getRandom/utils.ts
+++ src/routes/api/v1/prizes/getRandom/utils.ts
@@ -1,8 +1,13 @@
-import prizes from "../prizes.json";
+import { Prizes } from "../../../../../models";
+// import prizes from "../prizes.json";
 
-export const getRandomPrize = () => {
-	const prizeArray = prizes
-		.reduce((pv, cv) => [...pv, ...cv.prizes], [])
-		.filter((prize) => prize.available);
+export const getRandomPrize = async () => {
+	// const prizeArray = prizes
+	// 	.reduce((pv, cv) => [...pv, ...cv.prizes], [])
+	// 	.filter((prize) => prize.available);
+	// return prizeArray[Math.floor(Math.random() * prizeArray.length)];
+
+	const prizeArray = await Prizes.find();
+	console.log(prizeArray);
 	return prizeArray[Math.floor(Math.random() * prizeArray.length)];
 };
